6cc91ef97b700dd6e36b39c729b46f1f225881ed
aayushranjan25 <aayushranjan25@outlook.com>
2026-03-18 17:06:56 +0000

    updated cleanInput function

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,26 @@
+import { createInterface } from "node:readline";
+import { workerData } from "node:worker_threads";
+
 export function cleanInput(input: string): string[] {
-    const cleanedInput = input.trim().toLowerCase().split(" ");
+    const cleanedInput = input.trim().toLowerCase().split(" ").filter((word) => word !== "");
     return cleanedInput;
 }
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex >",
+        });
+
+    rl.prompt();
+    rl.on("line",(input: string) => {
+        const cleanedInput = cleanInput(input);
+        if(cleanedInput.length === 0) {
+            rl.prompt();
+            return;
+        }
+        console.log(`Your command was: ${cleanedInput[0]}`);            
+        rl.prompt();
+        });
+}
+
